07032b73403539f49fc3743b24aef781f57a2365
niranjanpansare <niranjanpansare@gmail.com>
2022-07-14 11:23:34 +0530

    deploy

--- pages/9_📊_Data_Profiling.py
+++ pages/9_📊_Data_Profiling.py
@@ -14,6 +14,7 @@ hide_streamlit_style = """
             
             </style>
             """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 def get_filesize(file):
     size_bytes = sys.getsizeof(file)
